ef84eeca6f4d0df41ad39462ae2752909854dca9
SeniorPomidorDeveloper <ridick.dz@yandex.ru>
2024-03-07 01:10:58 +0300

    Update game.* files

--- game.c
+++ game.c
@@ -7,6 +7,43 @@
 #include "libs/gets.h"
 #include "libs/compares.h"
 
+int init_field(Game *game, Field *field)
+{
+    field->points = (bool **) calloc(game->height, sizeof(bool *));
+    if (field->points == NULL)
+    {
+        return MEMORY_REALLOCATION_ERROR;
+    }
+    for (size_t i = 0; i < game->height; ++i) 
+    {
+        field->points[i] = (bool *) calloc(game->width, sizeof(bool));
+        if (field->points[i] == NULL)
+        {
+            return MEMORY_REALLOCATION_ERROR;
+        }
+    }
+    return SUCCESS;
+}
+
+void free_field(Game *game, Field *field)
+{
+    if (field != NULL)
+    {
+        if (field->points != NULL)
+        {
+            for (size_t j = 0; j < game->height; ++j)
+            {
+                if (field->points[j] != NULL)
+                {
+                    free(field->points[j]);
+                }
+            }
+            free(field->points);
+        }
+        free(field);
+    }
+}
+
 int create_new_point(Game *game, Field *field, Point *point)
 {
     if (point->x >= game->width || point->y >= game->height)
@@ -15,6 +52,165 @@ int create_new_point(Game *game, Field *field, Point *point)
     }
     field->points[point->y][point->x] = true;
     return SUCCESS;
+}
+
+unsigned short int count_neighbours(Game *game, Field *field, Point *point) 
+{
+    Point left_top = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
+                      (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
+    Point left = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
+                  point->y};
+    Point left_bottom = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
+                         (point->y + 1) % game->height};
+    Point right_top = {(point->x + 1) % game->width,  
+                       (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
+    Point right = {(point->x + 1) % game->width, 
+                   point->y};
+    Point right_bottom = {(point->x + 1) % game->width, 
+                          (point->y + 1) % game->height};
+    Point top = {point->x, (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
+    Point bottom = {point->x, (point->y + 1) % game->height};
+    const size_t len = 8;
+    Point *neighbours[] = {&left_top, &left, &left_bottom, &right_top, &right, &right_bottom, &top, &bottom};
+    unsigned short int count = 0;
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (field->points[neighbours[i]->y][neighbours[i]->x] == true)
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+void generate_new_points(Game *game, Field *old_field)
+{
+    for (size_t i = 0; i < game->height; ++i)
+    {
+        for (size_t j = 0; j < game->width; ++j)
+        {
+            Point point = {j, i};
+            unsigned short int count = count_neighbours(game, old_field, &point);
+            if (count == 3) 
+            {
+                game->active_field->points[i][j] = true;
+            } else if (count < 2 || count > 3) 
+            {
+                game->active_field->points[i][j] = false;
+            } else {
+                game->active_field->points[i][j] = old_field->points[i][j];
+            }
+        }
+    }
+}
+
+int to_next_gen(Game *game) // Доработать
+{   
+    Field *old_field = NULL;
+    if (game->active_field != NULL)
+    {
+        push_CyclicStack(game->history_field, &(game->active_field), &old_field);
+        if (is_full_CyclicStack(game->history_field))
+        {
+            free_field(game, old_field);
+        }
+        old_field = game->active_field;
+    }
+    game->active_field = (Field *) malloc(sizeof(Field));
+    if (game->active_field == NULL)
+    {
+        return MEMORY_REALLOCATION_ERROR;
+    }
+    if (init_field(game, game->active_field) == MEMORY_REALLOCATION_ERROR)
+    {
+        return MEMORY_REALLOCATION_ERROR;
+    }
+    if (old_field != NULL)
+    {
+        generate_new_points(game, old_field);
+    }
+    return SUCCESS;
+}
+
+void to_prev_gen(Game *game)
+{
+    if (!is_empty_CyclicStack(game->history_field)) 
+    {
+        free_field(game, game->active_field);
+        pop_CyclicStack(game->history_field, &(game->active_field));
+    }
+}
+
+
+void free_game(Game *game)
+{
+    while (!is_empty_CyclicStack(game->history_field)) 
+    {
+        Field *field;
+        pop_CyclicStack(game->history_field, &field);
+        free_field(game, field);
+    }
+    free_field(game, game->active_field);
+    free_CyclicStack(game->history_field);
+}
+
+int start_event(Game *game, int event) 
+{
+    switch (event)
+    {
+        case TO_NEXT_GEN:
+            if (to_next_gen(game) == MEMORY_REALLOCATION_ERROR) {
+                return MEMORY_REALLOCATION_ERROR;
+            }
+            break;
+        case TO_PREV_GEN:
+            to_prev_gen(game);
+            break;
+        default:
+        {
+            return NON_EXISTENT_EVENT;
+            break;
+        }
+    }
+    return SUCCESS;
+}
+
+int create_history(Game *game)
+{
+    if (init_CyclicStack(&(game->history_field), game->capacity, sizeof(Field *)) == MEMORY_REALLOCATION_ERROR)
+    {
+        return MEMORY_REALLOCATION_ERROR;
+    }
+    return SUCCESS;
+}
+
+void render(Game *game)
+{
+    for (size_t i = 0; i < game->height; ++i)
+    {
+        printf("+");
+        for (size_t j = 0; j < game->width; ++j) 
+        {
+            printf("-+");
+        }
+        printf("\n|");
+        for (size_t j = 0; j < game->width; ++j) 
+        {
+            if (game->active_field->points[i][j] == true)
+            {
+                printf("▉|");
+            } else
+            {
+                printf(" |");
+            }
+        }
+        printf("\n");
+    }
+    printf("+");
+    for (size_t j = 0; j < game->width; ++j) 
+    {
+        printf("-+");
+    }
+    printf("\n");
 }
 
 int input_points(Game *game, Field *field)
@@ -90,207 +286,6 @@ int command_menu()
         }
     } while (result == INVALID_COMMAND);
     return result;
-}
-
-unsigned short int count_neighbours(Game *game, Field *field, Point *point) 
-{
-    Point left_top = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
-                      (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
-    Point left = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
-                  point->y};
-    Point left_bottom = {(size_t) (((long int) point->x - 1 < 0) * (game->width - 1)) + (size_t) (((long int) point->x - 1 >= 0) * (point->x - 1)), 
-                         (point->y + 1) % game->height};
-    Point right_top = {(point->x + 1) % game->width,  
-                       (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
-    Point right = {(point->x + 1) % game->width, 
-                   point->y};
-    Point right_bottom = {(point->x + 1) % game->width, 
-                          (point->y + 1) % game->height};
-    Point top = {point->x, (size_t) (((long int) point->y - 1 < 0) * (game->height - 1)) + (size_t) (((long int) point->y - 1 >= 0) * (point->y - 1))};
-    Point bottom = {point->x, (point->y + 1) % game->height};
-    const size_t len = 8;
-    Point *neighbours[] = {&left_top, &left, &left_bottom, &right_top, &right, &right_bottom, &top, &bottom};
-    unsigned short int count = 0;
-    for (size_t i = 0; i < len; ++i)
-    {
-        if (field->points[neighbours[i]->y][neighbours[i]->x] == true)
-        {
-            ++count;
-        }
-    }
-    return count;
-}
-void generate_new_points(Game *game, Field *old_field)
-{
-    for (size_t i = 0; i < game->height; ++i)
-    {
-        for (size_t j = 0; j < game->width; ++j)
-        {
-            Point point = {j, i};
-            unsigned short int count = count_neighbours(game, old_field, &point);
-            if (count == 3) 
-            {
-                game->active_field->points[i][j] = true;
-            } else if (count < 2 || count > 3) 
-            {
-                game->active_field->points[i][j] = false;
-            } else {
-                game->active_field->points[i][j] = old_field->points[i][j];
-            }
-        }
-    }
-}
-
-int init_field(Game *game, Field *field)
-{
-    field->points = (bool **) calloc(game->height, sizeof(bool *));
-    if (field->points == NULL)
-    {
-        return MEMORY_REALLOCATION_ERROR;
-    }
-    for (size_t i = 0; i < game->height; ++i) 
-    {
-        field->points[i] = (bool *) calloc(game->width, sizeof(bool));
-        if (field->points[i] == NULL)
-        {
-            return MEMORY_REALLOCATION_ERROR;
-        }
-    }
-    return SUCCESS;
-}
-
-void free_field(Game *game, Field *field)
-{
-    if (field != NULL)
-    {
-        if (field->points != NULL)
-        {
-            for (size_t j = 0; j < game->height; ++j)
-            {
-                if (field->points[j] != NULL)
-                {
-                    free(field->points[j]);
-                }
-            }
-            free(field->points);
-        }
-        free(field);
-    }
-}
-
-int to_next_gen(Game *game) // Доработать
-{   
-    Field *old_field = NULL;
-    if (game->active_field != NULL)
-    {
-        push_CyclicStack(game->history_field, &(game->active_field), &old_field);
-        if (is_full_CyclicStack(game->history_field))
-        {
-            free_field(game, old_field);
-        }
-        old_field = game->active_field;
-    }
-    game->active_field = (Field *) malloc(sizeof(Field));
-    if (game->active_field == NULL)
-    {
-        return MEMORY_REALLOCATION_ERROR;
-    }
-    if (init_field(game, game->active_field) == MEMORY_REALLOCATION_ERROR)
-    {
-        return MEMORY_REALLOCATION_ERROR;
-    }
-    if (old_field != NULL)
-    {
-        generate_new_points(game, old_field);
-    }
-    return SUCCESS;
-}
-
-void to_prev_gen(Game *game)
-{
-    if (!is_empty_CyclicStack(game->history_field)) 
-    {
-        free_field(game, game->active_field);
-        pop_CyclicStack(game->history_field, &(game->active_field));
-    }
-}
-
-int start_event(Game *game, int event) 
-{
-    switch (event)
-    {
-        case TO_NEXT_GEN:
-            if (to_next_gen(game) == MEMORY_REALLOCATION_ERROR) {
-                return MEMORY_REALLOCATION_ERROR;
-            }
-            break;
-        case TO_PREV_GEN:
-            to_prev_gen(game);
-            break;
-        default:
-        {
-            return NON_EXISTENT_EVENT;
-            break;
-        }
-    }
-    return SUCCESS;
-}
-
-void render(Game *game)
-{
-    for (size_t i = 0; i < game->height; ++i)
-    {
-        printf("+");
-        for (size_t j = 0; j < game->width; ++j) 
-        {
-            printf("-+");
-        }
-        printf("\n|");
-        for (size_t j = 0; j < game->width; ++j) 
-        {
-            if (game->active_field->points[i][j] == true)
-            {
-                printf("▉|");
-            } else
-            {
-                printf(" |");
-            }
-        }
-        printf("\n");
-    }
-    printf("+");
-    for (size_t j = 0; j < game->width; ++j) 
-    {
-        printf("-+");
-    }
-    printf("\n");
-}
-
-void free_game(Game *game)
-{
-    while (!is_empty_CyclicStack(game->history_field)) 
-    {
-        Field *field;
-        pop_CyclicStack(game->history_field, &field);
-        free_field(game, field);
-    }
-    free_field(game, game->active_field);
-    free_CyclicStack(game->history_field);
-}
-
-int create_history(Game *game)
-{
-    if (init_CyclicStack(&(game->history_field), game->capacity, sizeof(Field *)) == MEMORY_REALLOCATION_ERROR)
-    {
-        return MEMORY_REALLOCATION_ERROR;
-    }
-    // for (size_t i = 0; i < game->capacity; ++i)
-    // {
-    //     Field *field = (Field *) malloc(sizeof(Field));
-    //     field->points = NULL;
-    //     push_CyclicStack(game->history_field, &field, NULL);
-    // }
-    return SUCCESS;
 }
 
 int start_game(size_t width, size_t height, size_t capacity)
--- game.h
+++ game.h
@@ -45,19 +45,23 @@ typedef struct Game
     CyclicStack *history_field;
 } Game;
 
+int init_field(Game *game, Field *field);   // Инициализация игрового поля.
+void free_field(Game *game, Field *field);  // Отчистка памяти отведённого под игровое поле.
+
 int create_new_point(Game *game, Field *field, Point *point); // Создание новой живой точки на игровом поле на котором игрок находится в данный момен времени.
+unsigned short int count_neighbours(Game *game, Field *field, Point *point); 
+void generate_new_points(Game *game, Field *field); // Создание следующего поколения точек на игровом поле. 
 int to_next_gen(Game *game);                    // Переход к следующему поколению живых точек на игровом поле.
-void to_prev_gen(Game *game);                    // Переход к предыдущему поколению живых точек на игровом поле.
-void generate_new_points(Game *game, Field *field); // Создание следующего поколения точек на игровом поле.  
+void to_prev_gen(Game *game);                    // Переход к предыдущему поколению живых точек на игровом поле. 
+
+void free_game(Game *game); // Освобождение памяти, выделенной за время игры.
+int start_event(Game *game, int event); // Обработка событий.
+int create_history(Game *game); // Создание истории живых точек на игровом поле.
 
 void render(Game *game); // Рендер игрового поля на экране.
-
+int input_points(Game *game, Field *field); // Ввод живых точек на игровом поле.
 void print_menu(); // Вывод меню ввода команд.
 int command_menu(); // Меню ввода комманд.
-
-int start_event(Game *game, int event); // Обработка событий.
-int input_points(Game *game, Field *field); // Ввод живых точек на игровом поле.
-void free_game(Game *game);
 
 int start_game(size_t width, size_t height, size_t capacity); // Запуск игры.
 
